f7d94f43a720bc164fd6a068d5dfc668f1d68ad1
211_0@naver.com <211_0@naver.com>
2024-03-14 01:04:08 +0900

    build(init): React Native 프로젝트 초기화

---
+++ src/styles/costomTheme.js
@@ -0,0 +1,10 @@
+export const DefaultTheme = {
+  mainBlue: '#012CFF',
+  subBlue: '#2964E0',
+  pointYellow: '#A0E1C8',
+  
+  fontLarge: '',
+  fontMedium: '',
+  fontRegular: '',
+  fontSmall: '',
+};
