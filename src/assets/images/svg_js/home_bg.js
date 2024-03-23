c2436d5ef1dd1394be9f5231e25ffa20d88e3438
211_0@naver.com <211_0@naver.com>
2024-03-24 06:04:54 +0900

    style: homePage 제작을 위한 컴포넌트 제작 및 정리

---
+++ src/assets/images/svg_js/home_bg.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Svg, { Path } from 'react-native-svg';
+
+const HomeBg = (props) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    width='100%'
+    height='100%'
+    fill="none"
+    viewBox="0 -150 375 715"
+    {...props}
+  >
+    <Path
+      fill="#fff"
+      fillRule="evenodd"
+      d="M375 277.421V720H0V0c0 104.626 67.5 186 202.154 186 91.555 0 146.837 46.924 172.846 91.421Z"
+      clipRule="evenodd"
+    />
+  </Svg>
+)
+export default HomeBg;
---
+++ src/components/Homecard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { customTheme } from '../styles/customTheme.js';
+
+const Homecard = () => {
+  return (
+    <View style={styles.rectangle} />
+  );
+};
+
+const styles = StyleSheet.create({
+  rectangle: {
+    width: 260,
+    height: 360,
+    backgroundColor: customTheme.bgBasic,
+    borderRadius: 20,
+  },
+});
+
+export default Homecard;
