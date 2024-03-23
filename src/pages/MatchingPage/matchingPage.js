d113ff27d15f0a3992779d2f658a149d66fcac75
211_0@naver.com <211_0@naver.com>
2024-03-23 23:57:57 +0900

    style: 파일명 변경 및 바텀 네비게이션 작성을 위한 기본틀 작성

--- src/pages/MatchingPage/matchingPage.js
+++ src/pages/MatchingPage/matchingPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const chattingPage = () => {
+const matchingPage = () => {
   return (
     <View style={styles.container}>
       <Text>chattingPage</Text>
@@ -17,4 +17,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default chattingPage;
+export default matchingPage;
--- src/pages/memberPage/memberPage.js
+++ src/pages/memberPage/memberPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const chattingPage = () => {
+const memberPage = () => {
   return (
     <View style={styles.container}>
       <Text>chattingPage</Text>
@@ -17,4 +17,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default chattingPage;
+export default memberPage;
