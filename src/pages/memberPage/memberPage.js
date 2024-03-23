7c4adcf10b59a2cd5503be0b4db5d2384a20d9cd
211_0@naver.com <211_0@naver.com>
2024-03-24 01:42:58 +0900

    style: 화면에 뜨는 글자 변경

--- src/pages/memberPage/memberPage.js
+++ src/pages/memberPage/memberPage.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 const MemberPage = () => {
   return (
     <View style={styles.container}>
-      <Text>chattingPage</Text>
+      <Text>MemberPage</Text>
     </View>
   );
 }
