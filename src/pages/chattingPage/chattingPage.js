9205d6dc767b112769fe88933e5af14b44effc22
211_0@naver.com <211_0@naver.com>
2024-03-24 01:21:34 +0900

    style: 컴포넌트명 첫 글자 대문자로 변경

--- src/pages/chattingPage/chattingPage.js
+++ src/pages/chattingPage/chattingPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const chattingPage = () => {
+const ChattingPage = () => {
   return (
     <View style={styles.container}>
       <Text>chattingPage</Text>
--- src/pages/commuityPage/commuityPage.js
+++ src/pages/commuityPage/commuityPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const communityPage = () => {
+const CommunityPage = () => {
   return (
     <View style={styles.container}>
       <Text>chattingPage</Text>
@@ -17,4 +17,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default communityPage;
+export default CommunityPage;
