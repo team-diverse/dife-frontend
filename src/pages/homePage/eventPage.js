0a631e32dc14fea56be4108f574fcfe45bbe083c
211_0@naver.com <211_0@naver.com>
2024-03-25 19:23:34 +0900

    style: eventPage의 flatList가 화면 차지하는 영역을 전체 영역으로 변경

--- src/pages/homePage/eventPage.js
+++ src/pages/homePage/eventPage.js
@@ -26,16 +26,24 @@ const EventPage = () => {
         },
         {
             id: '4',
-            title: '[프로그램] 해담이 탐구회',
-            date: '2022.11.13 ~ 2022.11.13',
-            eventImg: require('../../assets/images/test_img/test_haedam.jpg')
+            title: '[프로그램] 2022-1학기 해외파견교환학생 설명회 홍보',
+            date: '2022.02.14 ~ 2022.02.15',
+            eventImg: require('../../assets/images/test_img/test_event.png')
+        },
+        {
+            id: '5',
+            title: '[프로그램] 2022-1학기 해외파견교환학생 설명회 홍보',
+            date: '2022.02.14 ~ 2022.02.15',
+            eventImg: require('../../assets/images/test_img/test_event2.png')
         },
     ];
 
     return (
         <SafeAreaView style={[EventStyles.container, { alignItems: 'center' }]}>
             <TopBar tobBar="이벤트"/>
+            <View style={EventStyles.flatlist}>
             <FlatList
+            contentContainerStyle={EventStyles.flatlistContent}
                 data={eventData}
                 renderItem={({ item }) => (
                     <EventCard
@@ -46,6 +54,7 @@ const EventPage = () => {
                 )}
                 keyExtractor={item => item.id}
             />
+            </View>
         </SafeAreaView>
     )
 }
