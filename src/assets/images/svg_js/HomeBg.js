86562ebc135d4196324c74fbd2041423dbab17ad
211_0@naver.com <211_0@naver.com>
2024-03-26 17:28:46 +0900

    style: homePage의 홈 카드 끈 이미지 위치에 맞게 하얀색 배경 이미지 위치 변경

--- src/assets/images/svg_js/HomeBg.js
+++ src/assets/images/svg_js/HomeBg.js
@@ -7,7 +7,7 @@ const HomeBg = (props) => (
     width='100%'
     height='100%'
     fill="none"
-    viewBox="0 -100 375 715"
+    viewBox="0 -100 375 910"
     {...props}
   >
     <Path
@@ -15,6 +15,10 @@ const HomeBg = (props) => (
       fillRule="evenodd"
       d="M375 277.421V720H0V0c0 104.626 67.5 186 202.154 186 91.555 0 146.837 46.924 172.846 91.421Z"
       clipRule="evenodd"
+    />
+    <Path
+      fill="#fff"
+      d="M0 400h375v500H0z"
     />
   </Svg>
 )
