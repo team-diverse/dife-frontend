d79d92ac57bd355b832ac8fc28bccd5dc665b171
211_0@naver.com <211_0@naver.com>
2024-03-26 16:42:32 +0900

    style: eventPage의 이벤트카드  그림자 추가 및 흰색 부분 radius 변경

--- src/pages/HomePages/EventStyles.js
+++ src/pages/HomePages/EventStyles.js
@@ -13,6 +13,12 @@ const EventStyles = StyleSheet.create({
     flatlistContent: {
         alignItems: 'center',
     },
+    eventCard: {
+        shadowColor: '#3C454E4A',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.71,
+        shadowRadius: 6,
+    },
 });
 
 export default EventStyles;
