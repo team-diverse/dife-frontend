413fa281f8e25895da7a0efd2ec234079637b2bb
nnyouung <104901660+nnyouung@users.noreply.github.com>
2024-03-31 03:00:25 +0900

    feat: 상단바 컴포넌트 생성 및 적용, eventPage 구현
    * feat: 폰트 사용을 위한 폰트 로드 코드 추가
    
    * style: homePage 제작을 위한 컴포넌트 제작 및 정리
    
    * feat: homePage 기본 틀 구현
    
    * style: component 폴더 정리
    
    * feat: homepage 기본 틀 완성
    
    * feat: homepage 카드 뒷면 틀 완성
    
    * refactor: styleSheet 다른 파일로 분리

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { customTheme } from './src/styles/customTheme.js';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from 'expo-font';
 
 import ChattingPage from './src/pages/chattingPage/chattingPage.js';
 import MatchingPage from './src/pages/matchingPage/matchingPage.js';
@@ -9,15 +10,37 @@ import HomePage from './src/pages/homePage/homePage.js';
 import CommunityPage from './src/pages/commuityPage/commuityPage.js';
 import MemberPage from './src/pages/memberPage/memberPage.js';
 
-import ChatDf24 from './src/components/icon_24/svg_js/chat_df_24.js';
-import ConnectDf24 from './src/components/icon_24/svg_js/connect_df_24.js';
-import HomeDf24 from './src/components/icon_24/svg_js/home_df_24.js';
-import CommuDf24 from './src/components/icon_24/svg_js/commu_df_24.js';
-import MyDf24 from './src/components/icon_24/svg_js/my_df_24.js';
+import ChatDf24 from './src/components/icon_24/chat_df_24.js';
+import ConnectDf24 from './src/components/icon_24/connect_df_24.js';
+import HomeDf24 from './src/components/icon_24/home_df_24.js';
+import CommuDf24 from './src/components/icon_24/commu_df_24.js';
+import MyDf24 from './src/components/icon_24/my_df_24.js';
+
+import EventPage from './src/pages/homePage/eventPage.js';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomePage} options={{headerShown: false}} />
+      <Stack.Screen name="Event" component={EventPage} options={{headerShown: false}} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
+  const [loaded] = useFonts({
+    'NotoSansCJKkr-Bold': require('./src/assets/fonts/NotoSansCJKkr-Bold.otf'),
+    'NotoSansCJKkr-Medium': require('./src/assets/fonts/NotoSansCJKkr-Medium.otf'),
+    'NotoSansCJKkr-Regular': require('./src/assets/fonts/NotoSansCJKkr-Regular.otf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Tab.Navigator initialRouteName="homePage"
@@ -44,7 +67,7 @@ export default function App() {
       >
         <Tab.Screen name="chattingPage" component={ChattingPage} options={{ headerShown: false }} />
         <Tab.Screen name="matchingPage" component={MatchingPage} options={{ headerShown: false }} />
-        <Tab.Screen name="homePage" component={HomePage} options={{ headerShown: false }} />
+        <Tab.Screen name="homePage" component={HomeStack} options={{ headerShown: false }} />
         <Tab.Screen name="communityPage" component={CommunityPage} options={{ headerShown: false }} />
         <Tab.Screen name="memberPage" component={MemberPage} options={{ headerShown: false }} />
       </Tab.Navigator>
