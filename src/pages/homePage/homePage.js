77fef5ae174f3b177c280c062c682487b2bc1de4
nnyouung <104901660+nnyouung@users.noreply.github.com>
2024-04-03 02:53:59 +0900

    feat: eventPage 기본 틀 구현
    * feat: stack navigation 기능 추가 및 eventpage 관련 파일 생성
    
    * refactor: 상태바 높이 고려하기 위해 SafeAreaView 추가
    
    * feat: 상단바 컴포넌트 생성 및 eventPage 적용
    
    * feat: EventCard 컴포넌트 제작 및 EventPage에 적용
    
    * feat: eventPage 스크롤 구현

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+
+import HomeStyles from './homeStyles.js';
+
+import { useNavigation } from '@react-navigation/native';
 import { View, Text, StatusBar } from 'react-native';
 import HomeStyles from './homeStyles.js';
 
@@ -23,6 +28,9 @@ import HomecardBackBtn from '../../components/homeCompo/HomecardBackBtn.js';
 const HomePage = ({introduion = "adipiscing varius eu sit nulla, luctus tincidunt ex at ullamcorper cursus odio laoreet placerat.",
     name = "Amyyheart",
     country="France",
+    age="23",
+    }) => {
+  const navigation = useNavigation();
     age="23"}) => {
   const statusBarHeight = StatusBar.currentHeight || 0;
 
@@ -33,8 +41,8 @@ const HomePage = ({introduion = "adipiscing varius eu sit nulla, luctus tincidun
   };
 
   return (
-    <View style={HomeStyles.container}>
-      <LinearGradient style={[HomeStyles.linearGradient, { paddingTop: statusBarHeight }]} colors={['#0029F4', '#6199C1', '#6199C1']}>
+    <SafeAreaView style={HomeStyles.container}>
+      <LinearGradient style={HomeStyles.linearGradient} colors={['#0029F4', '#6199C1', '#6199C1']}>
         <HomeBg style={HomeStyles.svg} preserveAspectRatio='none'/>
         
         <View style={HomeStyles.topContainer}>
@@ -115,14 +123,14 @@ const HomePage = ({introduion = "adipiscing varius eu sit nulla, luctus tincidun
           <HomeSchEv />
           <Text style={HomeStyles.TXhomeSch}>학교정보</Text>
           <HomeSchoolInfo style={HomeStyles.homeSchIcon}/>
-          <View style={HomeStyles.homeEv}>
+          <TouchableOpacity style={HomeStyles.homeEv} onPress={() => navigation.navigate('Event')}>
             <HomeSchEv />
             <Text style={HomeStyles.TXhomeEv}>이벤트</Text>
             <HomeEvent style={HomeStyles.homeEvIcon}/>
-          </View>
+          </TouchableOpacity>
         </View>
       </LinearGradient>
-    </View>
+    </SafeAreaView>
   );
 };
 
