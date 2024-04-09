6dd730634ba8e6b6885730d425e064330041c1b6
nnyouung <211_0@kookmin.ac.kr>
2024-04-10 04:13:26 +0900

    style: connectPage 프로필의 배경 높이 변경

--- src/components/ConnectCompo/ConnectProfileBackground.js
+++ src/components/ConnectCompo/ConnectProfileBackground.js
@@ -3,9 +3,9 @@ import * as React from 'react';
 import Svg, { Circle, Rect } from 'react-native-svg';
 
 const ConnectProfileBackground = (props) => (
-    <Svg height="600" width="576">
+    <Svg height="1500" width="576">
         <Circle cx="288" cy="288" r="288" fill="white" />
-        <Rect x="0" y="80" width="576" height="616.691" fill="white" />
+        <Rect x="0" y="80" width="576" height="1500" fill="white" />
     </Svg>
 )
 export default ConnectProfileBackground;
