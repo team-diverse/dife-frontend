d5d3bf598648079c69263c107c2a706ea6d2347d
nnyouung <211_0@kookmin.ac.kr>
2024-04-14 03:42:27 +0900

    feat: loginPage 기본 구현

---
+++ App.js
@@ -0,0 +1,128 @@
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from 'expo-font';
+
+import ChattingPage from '@pages/ChattingPages/ChattingPage.js';
+import ConnectPage from '@pages/ConnectPages/ConnectPage.js';
+import HomePage from '@pages/HomePages/HomePage.js';
+import CommunityPage from '@pages/CommuityPages/CommuityPage.js';
+import MemberPage from '@pages/MemberPages/MemberPage.js';
+
+import ChatDf24 from '@components/Icon24/ChatDf24.js';
+import ConnectDf24 from '@components/Icon24/ConnectDf24.js';
+import HomeDf24 from '@components/Icon24/HomeDf24.js';
+import CommuDf24 from '@components/Icon24/CommuDf24.js';
+import MyDf24 from '@components/Icon24/MyDf24.js';
+
+import ChatAc32 from '@components/Icon32/ChatAc32.js';
+import ConnectAc32 from '@components/Icon32/ConnectAc32.js';
+import HomeAc32 from '@components/Icon32/HomeAc32.js';
+import CommuAc32 from '@components/Icon32/CommuAc32.js';
+import MyAc32 from '@components/Icon32/MyAc32.js';
+
+import EventPage from '@pages/HomePages/EventPage.js';
+import NotificationPage from '@pages/HomePages/NotificationPage.js';
+import ConnectLikeUserPage from '@pages/ConnectPages/ConnectLikeUserPage';
+import ConnectProfilePage from '@pages/ConnectPages/ConnectProfilePage';
+import LoginPage from 'src/pages/LoginPages/LoginPage';
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function LoginStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={LoginPage} options={{headerShown: false}} />
+    </Stack.Navigator>
+  )
+}
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomePage} options={{headerShown: false}} />
+      <Stack.Screen name="Event" component={EventPage} options={{headerShown: false}} />
+      <Stack.Screen name="Notification" component={NotificationPage} options={{headerShown: false}} />
+    </Stack.Navigator>
+  );
+}
+
+function ConnectStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Connect" component={ConnectPage} options={{headerShown: false}} />
+      <Stack.Screen name="ConnectLikeUser" component={ConnectLikeUserPage} options={{headerShown: false}} />
+      <Stack.Screen name="ConnectProfile" component={ConnectProfilePage} options={{headerShown: false}} />
+      {/* ConnectProfile 페이지 연결은 임시로.. 각 프로필 연결 시, 삭제하거나 수정 */}
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  const [loaded] = useFonts({
+    'NotoSansCJKkr-Bold': require('./src/assets/fonts/NotoSansCJKkr-Bold.otf'),
+    'NotoSansCJKkr-Medium': require('./src/assets/fonts/NotoSansCJKkr-Medium.otf'),
+    'NotoSansCJKkr-Regular': require('./src/assets/fonts/NotoSansCJKkr-Regular.otf'),
+  });
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer>
+      {isLoggedIn ? (
+      <Tab.Navigator initialRouteName="homePage"
+        screenOptions={({ route }) => ({
+          tabBarStyle: {
+            height: 90,
+            marginBottom: -10 },
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconComponent;
+
+            if (route.name === 'chattingPage') {
+              iconComponent = focused
+              ? <ChatAc32 width={32} height={32} fill={color} />
+              : <ChatDf24 width={size} height={size} fill={color} />;
+            } else if (route.name === 'connectPage') {
+              iconComponent = focused
+              ? <ConnectAc32 width={32} height={32} fill={color} />
+              : <ConnectDf24 width={size} height={size} fill={color} />;
+            } else if (route.name === 'homePage') {
+              iconComponent = focused
+              ? <HomeAc32 width={32} height={32} fill={color} />
+              : <HomeDf24 width={size} height={size} fill={color} />;
+            } else if (route.name === 'communityPage') {
+              iconComponent = focused
+              ? <CommuAc32 width={32} height={32} fill={color} />
+              : <CommuDf24 width={size} height={size} fill={color} />;
+            } else if (route.name === 'memberPage') {
+              iconComponent = focused
+              ? <MyAc32 width={32} height={32} fill={color} />
+              : <MyDf24 width={size} height={size} fill={color} />;
+            }
+
+            return iconComponent;
+          },
+          tabBarLabel: () => null,
+          
+        })}
+      >
+          <Tab.Screen name="chattingPage" component={ChattingPage} options={{ headerShown: false }} />
+          <Tab.Screen name="connectPage" component={ConnectStack} options={{ headerShown: false }} />
+          <Tab.Screen name="homePage" component={HomeStack} options={{ headerShown: false }} />
+          <Tab.Screen name="communityPage" component={CommunityPage} options={{ headerShown: false }} />
+          <Tab.Screen name="memberPage" component={MemberPage} options={{ headerShown: false }} />
+        </Tab.Navigator>
+        ) : (
+          <Stack.Navigator>
+            <Tab.Screen name="loginPage" component={LoginStack} options={{ headerShown: false }} />
+          </Stack.Navigator>
+        )}
+    </NavigationContainer>
+  );
+}
---
+++ src/components/LoginCompo/IconSeePw.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Svg, { Path } from 'react-native-svg';
+
+const IconSeePw = (props) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+  >
+    <Path
+      stroke="#393A3F"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.44}
+      d="M11.998 19.269c3.557 0 6.81-2.558 8.642-6.82-1.832-4.26-5.085-6.818-8.642-6.818-3.553 0-6.806 2.557-8.638 6.819 1.832 4.263 5.085 6.819 8.642 6.819h-.004Z"
+    />
+    <Path
+      stroke="#393A3F"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.44}
+      d="M14.956 12.455a2.954 2.954 0 1 1-5.908-.002 2.954 2.954 0 0 1 5.908.002Z"
+      clipRule="evenodd"
+    />
+  </Svg>
+)
+export default IconSeePw;
