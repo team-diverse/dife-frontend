d6eee87389860ae46bb21f506060494cc0ac517b
nnyouung <104901660+nnyouung@users.noreply.github.com>
2024-05-01 17:03:04 +0900

    Feat/connectpage (#20)
    * feat: 폰트 사용을 위한 폰트 로드 코드 추가
    
    * style: homePage 제작을 위한 컴포넌트 제작 및 정리
    
    * feat: homePage 기본 틀 구현
    
    * style: component 폴더 정리
    
    * feat: homepage 기본 틀 완성
    
    * feat: homepage 카드 뒷면 틀 완성
    
    * refactor: styleSheet 다른 파일로 분리
    
    * feat: stack navigation 기능 추가 및 eventpage 관련 파일 생성
    
    * refactor: 상태바 높이 고려하기 위해 SafeAreaView 추가
    
    * feat: 상단바 컴포넌트 생성 및 eventPage 적용
    
    * feat: EventCard 컴포넌트 제작 및 EventPage에 적용
    
    * feat: eventPage 스크롤 구현
    
    * feat: notificationPage 상단바 적용 및 homePage에서 notificatinPage로 이동하는 기능 구현
    
    * feat: notificationPage 기본 틀 완성
    
    * style: notificationCard svg 이미지 변경
    
    * style: eventPage의 flatList가 화면 차지하는 영역을 전체 영역으로 변경
    
    * style: eventCard 글자 부분 투명도 변경
    
    * style: introdution -> introduction 오타 수정
    
    * fix: 폰트 적용하는 과정에서의 에러 수정
    
    * refactor: 파일명과 객체명 일치 및 안 쓰이는 부분 정리
    
    * style: introduction 오타 추가 수정
    
    * feat: homePage의 알림 개수 구현
    
    * feat: homePage 소개 카드 양옆으로 이동 가능하게 구현
    
    * fix: 오타로 인한 경로 오류 수정
    
    * style: eventPage의 이벤트카드  그림자 추가 및 흰색 부분 radius 변경
    
    * style: homePage의 홈카드 그림자 투명도 변경
    
    * style: homePage의 홈카드 뒷면 끈 이미지 변경
    
    * style: homePage의 홈카드 뒷면 프로필 크기 조정
    
    * style: homePage의 홈 카드 끈 이미지 위치에 맞게 하얀색 배경 이미지 위치 변경
    
    * style: tab navigation 아이콘 유동적으로 바뀌도록 수정
    
    * fix: 알림 페이지 icon 값이 없을 때 alt 안 뜨는 부분 해결 및 기본값 영어로 변경
    
    * fix: homePage의 홈 카드에서 alt 안 뜨는 부분 수정
    
    * fix: eventPage의 이벤트 카드에서 alt 안 뜨는 부분 수정
    
    * style: path alias 설정
    
    * style: 파일명 PascalCase로 변경
    
    * style: Matching -> Connect로 파일 및 기능명 변경
    
    * feat: connectPage 검색창 컴포넌트 제작
    
    * feat: connectPage 검색창 부분 구현
    
    * feat: connectPage 화면 컴포넌트 제작
    
    * style: HomeTag -> Tag 파일 및 객체명 변화로 인한 수정
    
    * feat: connectCard 컴포넌트 완성
    
    * feat: connectPage 화면 부분 구현

---
+++ src/components/ConnectCompo/ConnectTop.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { CustomTheme } from '@styles/CustomTheme.js';
+
+const ConnectTop = () => {
+  return (
+    <View style={styles.rectangle} />
+  );
+};
+
+const styles = StyleSheet.create({
+  rectangle: {
+    width: '100%',
+    height: 175,
+    backgroundColor: CustomTheme.primaryMedium,
+    borderBottomLeftRadius: 40,
+    borderBottomRightRadius: 40,
+  },
+});
+
+export default ConnectTop;
---
+++ src/pages/ConnectPages/ConnectStyles.js
@@ -0,0 +1,117 @@
+import { StyleSheet } from 'react-native';
+import { CustomTheme } from '@styles/CustomTheme.js';
+
+const { fontHead20, fontHead18, fontCaption } = CustomTheme;
+
+const ConnectStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: CustomTheme.bgBasic,
+    },
+    topDifeContainer: {
+        alignItems: 'center',
+    },
+    connectTop: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+    },
+    connectDife: {
+        alignItems: 'center',
+        marginTop: -1,
+    },
+    safeAreaView: {
+        flex: 1,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+    },
+    textIconContainer: {
+        flexDirection: 'row',
+        position: 'absolute',
+        alignItems: 'center',
+        marginTop: 55,
+    },
+    connectTitle: {
+        flex: 1,
+        ...fontHead20,
+        color: CustomTheme.primaryBg,
+        marginLeft: 24,
+    },
+    addUserIcon: {
+        flex: 1,
+        marginRight: 24,
+    },
+    searchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        position: 'absolute',
+        marginTop: 100,
+    },
+    searchFilter: {
+        flex: 1,
+        marginLeft: 20,
+    },
+    searchIconContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flex: 1,
+        justifyContent: 'flex-end',
+        marginLeft: 12,
+        marginRight: 24,
+    },
+    search: {
+        position: 'relative',
+    },
+    searchIcon: {
+        position: 'absolute',
+        right: 12,
+    },
+    midContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 145,
+    },
+    tabContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        marginLeft: 30,
+    },
+    TXtab: {
+        ...fontHead18,
+        color: '#8C8D91',
+        textDecorationLine: 'underline',
+        marginRight: 18,
+    },
+    resetContainer: {
+        flexDirection: 'row',
+        marginRight: 30,
+        alignItems: 'center',
+    },
+    TXreset: {
+        ...fontCaption,
+        color: CustomTheme.primaryMedium,
+    },
+    flatlist: {
+        flex: 1,
+        marginTop: 22,
+        width: '100%',
+    },
+    flatlistContent: {
+        alignItems: 'center',
+    },
+    cardContainer: {
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginHorizontal: 25,
+        shadowColor: '#3C454E4A',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.71,
+        shadowRadius: 6,
+    },
+});
+
+export default ConnectStyles;
