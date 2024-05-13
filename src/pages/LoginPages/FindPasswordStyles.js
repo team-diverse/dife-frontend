73fecb5cdfe03f47062314db15a88ce142f24e9e
nnyouung <211_0@kookmin.ac.kr>
2024-05-13 19:16:45 +0900

    feat: 비밀번호 재발급 백엔드 연결 및 입력창 외 터치 시 키보드 내려가는 기능 추가

---
+++ src/pages/LoginPages/FindPasswordStyles.js
@@ -0,0 +1,58 @@
+import { StyleSheet } from 'react-native';
+import { CustomTheme } from '@styles/CustomTheme.js';
+
+const { fontHead24, fontSub16, fontSub14, fontCaption } = CustomTheme;
+
+const FindPasswordStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: CustomTheme.bgBasic,
+    },
+    iconArrow: {
+        marginTop: 5,
+        marginLeft: 14,
+    },
+    textTitle: {
+        ...fontHead24,
+        marginTop: 28,
+        marginLeft: 24,
+    },
+    textSubTitle: {
+        ...fontSub16,
+        color: CustomTheme.textSecondary,
+        marginTop: 12,
+        marginLeft: 22,
+    },
+    textId: {
+        ...fontSub14,
+        color: CustomTheme.textPrimary,
+        marginTop: 120,
+        marginLeft: 24,
+    },
+    textInputId: {
+        width: 327,
+        height: 44,
+        padding: 12,
+        borderWidth: 1,
+        borderColor: CustomTheme.borderColor,
+        borderRadius: 6,
+        marginTop: 8,
+        marginHorizontal: 25,
+        alignItems: 'center',
+    },
+    containerNotMember: {
+        flexDirection: 'row',
+        marginTop: 8,
+        marginLeft: 25,
+    },
+    textNotMember: {
+        ...fontCaption,
+        color: '#FF3E3E',
+        marginLeft: 3,
+    },
+    buttonPasswordReissue: {
+        marginTop: 37,
+    }
+});
+
+export default FindPasswordStyles;
