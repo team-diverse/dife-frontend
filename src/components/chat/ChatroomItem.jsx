806363197b594bc38cc1c43674090c268ba51fe2
Seungho Lee <poream3387@gmail.com>
2024-06-04 21:45:21 +0900

    feat(chat): 컴포넌트 이름 변경 및 채팅방 id 전달
    수정 사항
    
    - ChatRoomList -> ChatroomItem 컴포넌트 이름 변경
    - 채팅방 id를 넘겨줌 -> navigate에서 id 값을 채팅방에 전달해주기 위해

---
+++ src/components/chat/ChatroomItem.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { CustomTheme } from "@styles/CustomTheme";
+
+import IconChatProfile from "@components/chat/IconChatProfile";
+import { useNavigation } from "@react-navigation/native";
+
+const { fontCaption, fontNavi } = CustomTheme;
+
+const ChatroomItem = ({ id, context, name, time }) => {
+    const navigation = useNavigation("");
+
+    return (
+        <TouchableOpacity
+            style={styles.rectangle}
+            onPress={() =>
+                navigation.navigate("ChatRoomPage", {
+                    chatroomId: id,
+                })
+            }
+        >
+            <View style={styles.notify}>
+                <View style={styles.iconTextContainer}>
+                    <View style={styles.icon}>
+                        <IconChatProfile />
+                    </View>
+                    <View style={styles.textContainer}>
+                        <Text style={styles.textName}>{name}</Text>
+                        <Text style={styles.textContext}>{context}</Text>
+                    </View>
+                </View>
+                <Text style={styles.textTime}>{time}</Text>
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    rectangle: {
+        position: "relative",
+        width: "100%",
+        height: 80,
+        backgroundColor: CustomTheme.bgBasic,
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    notify: {
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "flex-start",
+        justifyContent: "space-between",
+    },
+    iconTextContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    icon: {
+        marginTop: 14,
+        marginBottom: 18,
+        marginLeft: 24,
+    },
+    textContainer: {
+        marginLeft: 22,
+    },
+    textName: {
+        fontSize: 14,
+        lineHeight: 17,
+        fontFamily: "NotoSansCJKkr-Bold",
+    },
+    textContext: {
+        ...fontCaption,
+        width: 187,
+        height: 34,
+        marginTop: 4,
+    },
+    textTime: {
+        ...fontNavi,
+        color: CustomTheme.textDisable,
+        marginTop: 14,
+        marginRight: 25,
+    },
+});
+
+export default ChatroomItem;
