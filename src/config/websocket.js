7ac1387dba397821907fb662933579c88187fe88
Seungho Lee <poream3387@gmail.com>
2024-06-12 15:48:29 +0900

    feat(chat): 백엔드에 웹소켓을 연결한다 (#46)
    개요 / 수정사항
    
    - StompJs를 사용해서 Backend에 웹소켓을 연결하자
    - websocket.js
      - 웹소켓 연결을 담당하는 Util 함수들을 모아두고 세팅을 하는 파일
      - 각종 핸들러를 정의 (연결됐을때, 에러났을 때 등)
      - 당분간 웹소켓 연결 확인이 쉽도록 console.log를 남겨놓고자함
    
    - React Native Web을 로컬에서 테스트용도로 돌리기 위해 아래의 package
    추가
      - metro-runtime
      - react-dom
      - react-native-web
    
    참고
    
    - 이후 PR에서 채팅을 받아오고 뿌리는 부분 구현 예정

---
+++ src/config/websocket.js
@@ -0,0 +1,27 @@
+import { Client } from '@stomp/stompjs';
+
+const WS_URL = 'ws://localhost:8080/ws';
+
+export const connectWebSocket = (onMessage) => {
+    let client = new Client({
+        brokerURL: WS_URL,
+        debug: (str) => {
+            console.log(str);
+        },
+        reconnectDelay: 0,
+         onConnect  : () => {
+            onMessage('Connected to WebSocket');
+        },
+        onStompError: (frame) => {
+            console.log('Broker reported error: ' + frame.headers['message']);
+            console.log('Additional details: ' + frame.body);
+        },
+        onWebSocketError: (error) => {
+            console.log('Websocket error: ' + error);
+        },
+        onWebSocketClose: (closeEvent) => {
+            console.log('Websocket connection closed');
+        },
+    });
+    client.activate();
+}
