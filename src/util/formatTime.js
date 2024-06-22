bc96fca3eae2ce6a08d3bf54c846c1f6bcd1af60
Seungho Lee <poream3387@gmail.com>
2024-06-23 01:01:42 +0900

    feat(chat): 채팅의 시간 표시 및 시간 형식 변환 코드 작성
    개요
    
    - 채팅의 시간을 형식에 맞게 표시한다.
    
    수정 사항
    
    - formatTime.js라는 util 스크립트를 만들어서 백엔드에서 ISO 포맷으로
    오는 코드를 "오후 XX:XX"의 형식으로 변환 함수 작성
    - ChatBubble에서 채팅 시간의 적절한 위치를 위해 새로운 View 추가 및
    marginTop:auto 부여 + width 늘려 text가 개행되지 않도록 함
    - ChattingPage와 ChattingRoomPage에서 formatTime 적용된 시간 보여줌
    - 아울러, message를 웹소켓으로 받을때, JSON.parse를 해서 시간적용 시
    에러가 나는 것을 해결함 (기존에 놓치고 있던 에러가 있었음)
    
    참고
    
    - 우선은 한국 시간으로만 변환하는 함수만 작성 이후에 변화에 유연하게
    작성 예정

---
+++ src/util/formatTime.js
@@ -0,0 +1,11 @@
+const formatKoreanTime = (isoString) => {
+    const date = new Date(isoString);
+    const formatter = new Intl.DateTimeFormat("ko-KR", {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+    });
+    return formatter.format(date);
+};
+
+export default formatKoreanTime;
