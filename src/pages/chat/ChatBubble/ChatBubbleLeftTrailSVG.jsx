a5d061c07291721c920e69fffb4320acb8582e77
Seungho Lee <poream3387@gmail.com>
2024-06-27 18:22:58 +0900

    feat(chat): 상대방 Chat Bubble 구현 (#59)
    개요
    
    - 상대방 채팅 버블을 구현한다.
    
    수정 사항
    
    - SVG 꼬리 부분 추가 및 이름 변경
    - ChatBubble에서 isMine 변수에 따라 상대방의 채팅 본인의 채팅인지
    구분하여 스타일링을 렌더링한다.
    - 일부 스타일 이름을 더 직관적이게 변경한다.
    - 참고로, flex-direction: row-reverse와 margin-auto를 사용해서 컴포넌트 위치를
    변경하는 것임

---
+++ src/pages/chat/ChatBubble/ChatBubbleLeftTrailSVG.jsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import Svg, { Path } from "react-native-svg";
+
+const ChatBubbleLeftTrailSVG = () => {
+    return (
+        <Svg
+            width="12"
+            height="35"
+            viewBox="0 0 12 35"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <Path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M2.11059 5.201C0.779367 7.13363 0 9.4758 0 12V23C0 29.6274 5.37258 35 12 35V0C9.57091 0 7.3104 0.721739 5.4212 1.96247L1.83373 0.396327C0.983197 0.0250184 0.111447 0.872552 0.493364 1.69946L2.11059 5.201Z"
+                fill="#EDEEF2"
+            />
+        </Svg>
+    );
+};
+
+export default ChatBubbleLeftTrailSVG;
