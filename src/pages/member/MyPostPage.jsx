5c8a58f6fff46b934258a28432f3a119d83b0676
nnyouung <211_0@kookmin.ac.kr>
2024-07-05 21:32:11 +0900

    feat: 내가 쓴 글 및 내가 단 댓글 페이지 기본 틀 구현
    수정사항
    - 내가 쓴 글 및 내가 단 댓글 페이지 기본 틀 구현
    - 내가 단 댓글 페이지 디자인 수정된 부분 추가

--- src/pages/member/MyPostPage.jsx
+++ src/pages/member/MyPostPage.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, TouchableOpacity } from 'react-native';
 
 import MyPostStyles from '@pages/member/MyPostStyles';
 import { CustomTheme } from '@styles/CustomTheme';
+import { useNavigation } from '@react-navigation/native';
 
 import TopBar from '@components/common/TopBar';
 import ArrowRight from '@components/common/ArrowRight';
 import ItemCommunity from '@components/community/ItemCommunity';
 
 const MyPostPage = () => {
+    const navigation = useNavigation();
+
     const MyPostList = [
         {title: '좋아요 화면입니다', content: '북악관 머시기저시기 와라라라라라랄 지나서...', heart: 2, bookmark: 2, comment: 0, created: '2024-06-22T10:30:00'},
         {title: '성곡도서관 가는 길', content: '북악관 머시기저시기 와라라라라라랄 지나서...', heart: 2, bookmark: 2, comment: 0, created: '2024-06-22T10:30:00'},
@@ -22,28 +25,28 @@ const MyPostPage = () => {
             <View style={{ marginTop: 14 }}>
                 <View style={MyPostStyles.containerTitle}>
                     <Text style={MyPostStyles.textTitle}>내가 쓴 글</Text>
-                    <View style={MyPostStyles.containerMore}>
+                    <TouchableOpacity style={MyPostStyles.containerMore} onPress={() => navigation.navigate('MyWrotePage')}>
                         <Text style={MyPostStyles.textMore}>더보기</Text>
                         <ArrowRight style={MyPostStyles.arrowRight} color={CustomTheme.textSecondary} size={18} />
-                    </View>
+                    </TouchableOpacity>
                 </View>
 
                 <View style={MyPostStyles.itemCommunity}>
-                <ItemCommunity props={MyPostList} />
+                    <ItemCommunity props={MyPostList} />
                 </View>
 
                 <View style={MyPostStyles.line} />
 
                 <View style={MyPostStyles.containerTitle}>
                     <Text style={MyPostStyles.textTitle}>내가 단 댓글</Text>
-                    <View style={MyPostStyles.containerMore}>
+                    <TouchableOpacity style={MyPostStyles.containerMore} onPress={() => navigation.navigate('MyCommentPage')}>
                         <Text style={MyPostStyles.textMore}>더보기</Text>
                         <ArrowRight style={MyPostStyles.arrowRight} color={CustomTheme.textSecondary} size={18} />
-                    </View>
+                    </TouchableOpacity>
                 </View>
 
                 <View style={MyPostStyles.itemCommunity}>
-                <ItemCommunity props={MyPostList} />
+                    <ItemCommunity props={MyPostList} comment={true}/>
                 </View>
             </View>
                 
