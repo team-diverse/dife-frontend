d76c9f1b6d4c0f23e2574ddfecaceacffb8420d1
nnyouung <211_0@kookmin.ac.kr>
2024-08-16 01:21:55 +0900

    feat: 회원탈퇴 구현
    수정사항
    - api.js
      - 회원 탈퇴 api 연결
    - LoginPage.js
      - 회원 탈퇴 후 다시 로그인 시, 탈퇴한 계정과 인증 대기를 구분하기 위해 조건문 추가
    - SecurityPage.jsx
      - 회원 탈퇴 구현

--- src/pages/member/SecurityPage.jsx
+++ src/pages/member/SecurityPage.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
-import { SafeAreaView, View, Text, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+	SafeAreaView,
+	View,
+	Text,
+	TouchableOpacity,
+	Alert,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import * as SecureStore from "expo-secure-store";
 
 import SecurityStyles from "@pages/member/SecurityStyles";
 import { CustomTheme } from "@styles/CustomTheme";
+import { useAuth } from "src/states/AuthContext";
+import { getMyProfile, updateMyProfile, deleteMember } from "config/api";
 
 import TopBar from "@components/common/TopBar";
 import ArrowRight from "@components/common/ArrowRight";
@@ -13,11 +22,94 @@ import IconSwitchOff from "@components/member/IconSwitchOff";
 const SecurityPage = () => {
 	const navigation = useNavigation();
 
+	const { setIsLoggedIn } = useAuth();
 	const [switchOn, setSwitchOn] = useState(false);
 
-	const handleSwitch = () => {
-		setSwitchOn(!switchOn);
+	const getIsPublic = async () => {
+		try {
+			const response = await getMyProfile();
+			setSwitchOn(!response.data.isPublic);
+		} catch (error) {
+			console.error(
+				"프로필 공개 여부 조회 오류:",
+				error.response ? error.response.data : error.message,
+			);
+		}
 	};
+
+	useEffect(() => {
+		getIsPublic();
+	}, []);
+
+	const handleSwitch = async () => {
+		try {
+			setSwitchOn(!switchOn);
+			const formData = new FormData();
+			formData.append("isPublic", switchOn);
+			await updateMyProfile(formData);
+		} catch (error) {
+			console.error(
+				"프로필 비공개 전환 오류:",
+				error.response ? error.response.data : error.message,
+			);
+		}
+	};
+
+	const handleLogout = async () => {
+		try {
+			await SecureStore.deleteItemAsync("memberId");
+			await SecureStore.deleteItemAsync("accessToken");
+			await SecureStore.deleteItemAsync("refreshToken");
+
+			setIsLoggedIn(false);
+		} catch (error) {
+			console.error("로그아웃 오류: ", error.message);
+		}
+	};
+
+	const handleAlertLogout = () => {
+		Alert.alert(
+			"",
+			"로그아웃하시겠습니까?",
+			[
+				{ text: "취소", style: "cancel" },
+				{
+					text: "확인",
+					onPress: () => {
+						handleLogout();
+					},
+				},
+			],
+			{ cancelable: false },
+		);
+	};
+
+	const handleDeleteMember = async () => {
+		try {
+			await deleteMember();
+			setIsLoggedIn(false);
+		} catch (error) {
+			console.error("회원 탈퇴 오류: ", error.message);
+		}
+	};
+
+	const handleAlertDeleteMember = () => {
+		Alert.alert(
+			"회원 탈퇴",
+			"정말 회원 탈퇴를 하시겠습니까?\n소중한 회원님을 잃게 되어 아쉽습니다.",
+			[
+				{ text: "취소", style: "cancel" },
+				{
+					text: "확인",
+					onPress: () => {
+						handleDeleteMember();
+					},
+				},
+			],
+			{ cancelable: false },
+		);
+	};
+
 	return (
 		<SafeAreaView style={SecurityStyles.container}>
 			<TopBar
@@ -57,7 +149,10 @@ const SecurityPage = () => {
 			<View style={SecurityStyles.line} />
 
 			<View style={SecurityStyles.containerContent}>
-				<TouchableOpacity style={SecurityStyles.backgroundWhite}>
+				<TouchableOpacity
+					style={SecurityStyles.backgroundWhite}
+					onPress={handleAlertLogout}
+				>
 					<View style={SecurityStyles.containerRow}>
 						<Text style={SecurityStyles.textContent}>로그아웃</Text>
 						<ArrowRight
@@ -67,7 +162,10 @@ const SecurityPage = () => {
 						/>
 					</View>
 				</TouchableOpacity>
-				<TouchableOpacity style={SecurityStyles.backgroundWhite}>
+				<TouchableOpacity
+					style={SecurityStyles.backgroundWhite}
+					onPress={handleAlertDeleteMember}
+				>
 					<View style={SecurityStyles.containerRow}>
 						<Text style={SecurityStyles.textContent}>
 							회원 탈퇴
