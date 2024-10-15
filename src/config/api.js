3d309d9a22a285664db3a4216988b7d4ca0c482b
suyeon <stgood@kookmin.ac.kr>
2024-10-15 16:43:13 +0900

    feat/chat-jwt-1: refreshToken으로 Auth 변경
    - 기존 accessToken으로만 가져왔던 부분 -> refreshToken으로 변경
    (AppConfig에 적용)

--- src/config/api.js
+++ src/config/api.js
@@ -10,9 +10,10 @@ export const api = axios.create({
 });
 
 api.interceptors.request.use(async (config) => {
-	const token = await SecureStore.getItemAsync("accessToken");
+	const token = await SecureStore.getItemAsync("refreshToken");
 	if (token) {
 		config.headers["Authorization"] = `Bearer ${token}`;
+		console.log("refreshToken : " + token)
 	}
 	return config;
 });
