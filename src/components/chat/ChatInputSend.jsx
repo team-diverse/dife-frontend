bf2544a2dbb6558b349c8618ea71a9c42fc4c6a5
sooyeon_nine <124496650+KooSuYeon@users.noreply.github.com>
2024-10-17 12:10:17 +0900

    Feat/chat jwt : 채팅에 JWT 입히기 & 넘치는 채팅 조절 (#152)
    * feat/chat-jwt-1: refreshToken으로 Auth 변경
    
    - 기존 accessToken으로만 가져왔던 부분 -> refreshToken으로 변경
    (AppConfig에 적용)
    
    * feat/chat-jwt-2: 채팅에 JWT 적용
    
    - SecureStore 이용해 refreshToken 가져온 후 가져온 token을 CONNECT/SUB/CHAT 시에 사용
    - 기존 채팅에서 가져왔던 memberId 삭제 및 SecurityContext 적용
    
    * feat/chat-jwt-3 : 장문 채팅 깨짐 문제 해결
    
    - 말풍선 보낸이에 따라 채팅 상단/하단에 위치할 지 설정 -> underlay, trailContainer
    - 겹칠 때에는 ChattingBubble이 우선시 되도록 zIndex 부여 (1이 우선)
    
    - 채팅 좌우 패딩 줄이기
    
    * feat/chat-jwt-4: run prettier
    
    * feat/chat-jwt-5: lint test
    
    * feat/chat-jwt-6: SecureStore로 들어오는 refreshToken 가져오기 함수화

--- src/components/chat/ChatInputSend.jsx
+++ src/components/chat/ChatInputSend.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
 	View,
 	TextInput,
@@ -12,13 +12,24 @@ import { CustomTheme } from "@styles/CustomTheme";
 
 import IconChatSend from "@components/chat/IconChatSend";
 import { useWebSocket } from "context/WebSocketContext";
+import { getRefreshToken } from "util/secureStoreUtils";
 
 const { fontBody14 } = CustomTheme;
 
-const ChatInputSend = ({ chatroomId, memberId }) => {
+const ChatInputSend = ({ chatroomId }) => {
 	const [chatInput, setChatInput] = useState("");
 	const [plusClick, setPlusClick] = useState(false);
 	const { publishMessage } = useWebSocket();
+	const [token, setToken] = useState(null);
+
+	useEffect(() => {
+		const fetchToken = async () => {
+			const token = await getRefreshToken();
+			setToken(token);
+		};
+
+		fetchToken();
+	}, []);
 
 	const handleInputFocus = () => {
 		if (plusClick) {
@@ -28,14 +39,16 @@ const ChatInputSend = ({ chatroomId, memberId }) => {
 
 	const handleSend = () => {
 		const trimmedChatInput = chatInput.trim();
-		if (trimmedChatInput) {
+		if (trimmedChatInput && token) {
 			publishMessage({
 				chatType: "CHAT",
 				chatroomId,
-				memberId,
 				message: trimmedChatInput,
+				token,
 			});
 			setChatInput("");
+		} else {
+			console.log("Token is missing or input is empty");
 		}
 	};
 
