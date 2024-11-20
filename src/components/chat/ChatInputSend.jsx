b4ed9b05f51125a1e3147d418d32bdfc95e9ba88
nnyouung <104901660+nnyouung@users.noreply.github.com>
2024-11-20 14:07:09 +0900

    Fix/chat improvements 2: 채팅방 생성 오류 해결, hold api 삭제, 웹소켓 연결 시 전체 채팅 메시지 로드 기능 제거, 상대방 채팅 프로필 이미지 표시 문제 해결 (#181)
    * fix: 채팅방 생성 오류 해결 및 불필요한 변수 삭제
    
    수정사항
    - WebSocketContext.js, createChatroom.js
      - 채팅방 생성 오류 해결
    - ChatRoomPage.jsx, ChatInputSend.jsx
      - 불필요한 변수 삭제
    
    * chore: 불필요한 hold api 삭제
    
    * fix: 웹소켓 연결 시 전체 채팅 메시지 로드 기능 제거 (unreadChatCounts 유지 목적)
    
    수정사항
    - createChatroom.js, WebSocketContext.js, FriendList.jsx, HomeCardFront.jsx, ItemRequestConnectList.jsx, ConnectProfilePage.js
      - 웹소켓 연결 시 전체 채팅 메시지 로드 기능 제거
      - 채팅방 진입 시 채팅 메시지 로드 기능 추가
    - ChatRoomPage.jsx
      - 불필요한 로그 제거
    
    * fix: 채팅 시간이 변경되는 시점에만 상대방의 프로필 이미지가 표시되던 문제 해결

--- src/components/chat/ChatInputSend.jsx
+++ src/components/chat/ChatInputSend.jsx
@@ -15,11 +15,10 @@ import IconChatSend from "@components/chat/IconChatSend";
 
 const { fontBody14 } = CustomTheme;
 
-const ChatInputSend = ({ chatroomId, isExited: initialIsExited, onFocus }) => {
+const ChatInputSend = ({ chatroomId, onFocus }) => {
 	const [chatInput, setChatInput] = useState("");
 	const { publishMessage } = useWebSocket();
 	const [token, setToken] = useState(null);
-	const [isExited, setIsExited] = useState(initialIsExited);
 
 	useEffect(() => {
 		const fetchToken = async () => {
@@ -33,22 +32,12 @@ const ChatInputSend = ({ chatroomId, isExited: initialIsExited, onFocus }) => {
 	const handleSend = async () => {
 		const trimmedChatInput = chatInput.trim();
 		if (trimmedChatInput && token) {
-			if (isExited) {
-				publishMessage({
-					chatType: "ENTER",
-					chatroomId,
-					message: trimmedChatInput,
-					token,
-				});
-				setIsExited(false);
-			} else {
-				publishMessage({
-					chatType: "CHAT",
-					chatroomId,
-					message: trimmedChatInput,
-					token,
-				});
-			}
+			publishMessage({
+				chatType: "CHAT",
+				chatroomId,
+				message: trimmedChatInput,
+				token,
+			});
 			setChatInput("");
 		} else {
 			console.log("Token is missing or input is empty");
