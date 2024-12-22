d7ec2f3332e27debeedbc3d183313b80571d8fe4
Seungho <poream3387@gmail.com>
2024-12-22 16:47:21 +0900

    fix(profile): 개인 프로필 이미지 로딩 수정
    - 개인 프로필 화면에서 로딩이 느린 부분 수정
    - SVG Image에서 expo image를 활용하기 위해 리팩토링 및 스타일 일부 수정
    - Caching 활성화 및 transition 설정
    - username placeholder를 설정해서 렌더링 시에 미리 UI 공간 확보해서
    무너지는 것 방지

--- src/components/member/MemberProfileBackground.jsx
+++ src/components/member/MemberProfileBackground.jsx
@@ -4,46 +4,63 @@ import Svg, {
 	Defs,
 	LinearGradient,
 	Stop,
-	Image as SvgImage,
 	ClipPath,
 } from "react-native-svg";
+import { Image } from "expo-image";
+import { View, StyleSheet } from "react-native";
 
 const MemberProfileBackground = ({ profileImage, ...props }) => (
-	<Svg
-		xmlns="http://www.w3.org/2000/svg"
-		width={116}
-		height={116}
-		fill="none"
-		{...props}
-	>
-		<Circle cx={58} cy={58} r={58} fill="url(#a)" />
+	<View style={styles.container}>
+		<Svg
+			xmlns="http://www.w3.org/2000/svg"
+			width={116}
+			height={116}
+			fill="none"
+			style={styles.svg}
+			{...props}
+		>
+			<Circle cx={58} cy={58} r={58} fill="url(#a)" />
+			<Defs>
+				<LinearGradient
+					id="a"
+					x1={108}
+					x2={3}
+					y1={116}
+					y2={-44}
+					gradientUnits="userSpaceOnUse"
+				>
+					<Stop stopColor="#B0D0FF" />
+				</LinearGradient>
+				<ClipPath id="clip">
+					<Circle cx={58} cy={58} r={58} />
+				</ClipPath>
+			</Defs>
+		</Svg>
 		{profileImage && (
-			<SvgImage
-				x={0}
-				y={0}
-				width={116}
-				height={116}
-				preserveAspectRatio="xMidYMid slice"
-				href={{ uri: profileImage }}
-				clipPath="url(#clip)"
+			<Image
+				source={{ uri: profileImage }}
+				style={styles.image}
+				cachePolicy="memory-disk"
+				transition={150}
 			/>
 		)}
-		<Defs>
-			<LinearGradient
-				id="a"
-				x1={108}
-				x2={3}
-				y1={116}
-				y2={-44}
-				gradientUnits="userSpaceOnUse"
-			>
-				<Stop stopColor="#B0D0FF" />
-			</LinearGradient>
-			<ClipPath id="clip">
-				<Circle cx={58} cy={58} r={58} />
-			</ClipPath>
-		</Defs>
-	</Svg>
+	</View>
 );
+
+const styles = StyleSheet.create({
+	container: {
+		width: 116,
+		height: 116,
+		position: "relative",
+	},
+	svg: {
+		position: "absolute",
+	},
+	image: {
+		width: 116,
+		height: 116,
+		borderRadius: 58,
+	},
+});
 
 export default MemberProfileBackground;
