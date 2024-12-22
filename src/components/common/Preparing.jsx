c89893ae08573fb5f2b0221c5eff3bc83730efc6
nnyouung <211_0@kookmin.ac.kr>
2024-12-22 19:58:30 +0900

    fix: Preparing 페이지에서 뒤로 갈 때 이미지가 넘치는 현상 수정

--- src/components/common/Preparing.jsx
+++ src/components/common/Preparing.jsx
@@ -4,7 +4,7 @@ import Svg, { Path } from "react-native-svg";
 const Preparing = (props) => (
 	<Svg
 		xmlns="http://www.w3.org/2000/svg"
-		width={401}
+		width="100%"
 		height={841}
 		fill="none"
 		{...props}
