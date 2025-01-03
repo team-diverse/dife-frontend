7788964c23e894a79f8666f4608e1ae3b916c621
suyeon <stgood@kookmin.ac.kr>
2025-01-03 10:42:38 +0900

    hotfix/delete-projectId-3 : 알림 권한 status 확인 추가

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -81,15 +81,23 @@ const LoginPage = () => {
 	const handleLogin = async () => {
 		try {
 			const loginResponse = await login(emailRef.val, valuePW);
-			const { status } = await Notifications.requestPermissionsAsync();
-
 			let token = "";
-			if (status === "granted") {
-				token = (await Notifications.getExpoPushTokenAsync()).data;
-			} else {
-				console.log("Push notification permissions not granted");
+			const { status: existingStatus } =
+				await Notifications.getPermissionsAsync();
+			let finalStatus = existingStatus;
+
+			if (existingStatus !== "granted") {
+				const { status } =
+					await Notifications.requestPermissionsAsync();
+				finalStatus = status;
 			}
 
+			if (finalStatus !== "granted") {
+				token = "undefined";
+				console.log("Push notification permissions not granted");
+			} else {
+				token = (await Notifications.getExpoPushTokenAsync()).data;
+			}
 			const id = loginResponse.data.member_id;
 			const accessToken = loginResponse.data.accessToken;
 			const refreshToken = loginResponse.data.refreshToken;
@@ -115,7 +123,10 @@ const LoginPage = () => {
 				navigation.navigate("Nickname");
 			}
 
-			await createNotificationToken(token, deviceId);
+			if (token) {
+				await createNotificationToken(token, deviceId);
+			}
+
 		} catch (error) {
 			Sentry.captureException(error);
 			console.error(
