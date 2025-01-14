3f65b872a6afd22d87911d8bccaed9116e52607c
suyeon <stgood@kookmin.ac.kr>
2025-01-15 03:21:03 +0900

    hotfix/login-posiible-1: 알림 동의 여부 저장하고 꺼낼 수 있도록 수정

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -81,16 +81,14 @@ const LoginPage = () => {
 	const handleLogin = async () => {
 		try {
 			const loginResponse = await login(emailRef.val, valuePW);
+			const status = await SecureStore.getItem(
+				"notificationPermissionStatus",
+			);
 			let token = "";
-			const { status: existingStatus } =
-				await Notifications.getPermissionsAsync();
-			let finalStatus = existingStatus;
-
-			if (finalStatus !== "granted") {
-				token = "undefined";
-				console.log("Push notification permissions not granted");
-			} else {
+			if (status === "granted") {
 				token = (await Notifications.getExpoPushTokenAsync()).data;
+			} else {
+				token = "undetined";
 			}
 			const id = loginResponse.data.member_id;
 			const accessToken = loginResponse.data.accessToken;
