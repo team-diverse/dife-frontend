18b68fdfb37cee10ead6c81da97f387648a1c9e4
sooyeon_nine <124496650+KooSuYeon@users.noreply.github.com>
2025-01-31 14:55:54 +0900

    hotfix/login-notification: 알림 권한 거부 시에도 로그인 페이지로 가도록 & staging과 알림 환경 동일하게(401방지) (#219)

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -79,17 +79,16 @@ const LoginPage = () => {
 	const handleLogin = async () => {
 		try {
 			const loginResponse = await login(emailRef.val, valuePW);
-			const status = await SecureStore.getItemAsync(
-				"notificationPermissionStatus",
-			);
+			const status = await Notifications.requestPermissionsAsync();
 
 			let token = "";
 			if (status === "granted") {
 				token = (await Notifications.getExpoPushTokenAsync()).data;
 			} else {
-				const status = (await Notifications.requestPermissionsAsync())
-					.granted;
-				if (status) {
+				const status_permission = (
+					await Notifications.requestPermissionsAsync()
+				).granted;
+				if (status_permission) {
 					token = (await Notifications.getExpoPushTokenAsync()).data;
 					console.log("FINALLY", token);
 				} else {
