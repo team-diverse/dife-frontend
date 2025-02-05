cd7d660fd472a5a262f8f03eceaeb15964347fa3
suyeon <stgood@kookmin.ac.kr>
2025-02-06 08:56:14 +0900

    hotfix/login-error-3: 로그인 에러 해결을 위한 실험 3
    - 알림 권한 확인 로직 동작하는지 확인

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -78,20 +78,21 @@ const LoginPage = () => {
 	const handleLogin = async () => {
 		try {
 			const loginResponse = await login(emailRef.val, valuePW);
-			// const { status } = await Notifications.getPermissionsAsync();
+			const { status } = await Notifications.getPermissionsAsync();
 
-			// let token = "";
-			// if (status === "granted") {
-			// 	token = (await Notifications.getExpoPushTokenAsync()).data;
-			// } else {
-			// 	const { granted } = await Notifications.requestPermissionsAsync();
-			// 	if (granted) {
-			// 		token = (await Notifications.getExpoPushTokenAsync()).data;
-			// 		console.log("FINALLY", token);
-			// 	} else {
-			// 		token = "undefined";
-			// 	}
-			// }
+			let token = "";
+			if (status === "granted") {
+				token = (await Notifications.getExpoPushTokenAsync()).data;
+			} else {
+				const { granted } =
+					await Notifications.requestPermissionsAsync();
+				if (granted) {
+					token = (await Notifications.getExpoPushTokenAsync()).data;
+					console.log("FINALLY", token);
+				} else {
+					token = "undefined";
+				}
+			}
 
 			const id = loginResponse.data.member_id;
 			const accessToken = loginResponse.data.accessToken;
@@ -117,10 +118,6 @@ const LoginPage = () => {
 				navigation.navigate("OnboardingPage");
 			}
 
-			// if (token) {
-			// 	await createNotificationToken(token, deviceId);
-			// }
-			const token = (await Notifications.getExpoPushTokenAsync()).data;
 			await createNotificationToken(token, deviceId);
 		} catch (error) {
 			Sentry.captureException(error);
