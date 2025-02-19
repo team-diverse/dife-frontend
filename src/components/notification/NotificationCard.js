4779527718035705193c38df7a0e85d2ff1419b9
sooyeon_nine <124496650+KooSuYeon@users.noreply.github.com>
2025-02-19 19:51:46 +0900

    refactor/notification-view-and-onboarding-hooby:알림 페이지 랜딩 변경 & 존재하지 않는 사용자 시 모달 표시 & 온보딩에서 취미 저장 형식 변경 (#228)
    * add/notification-background-1: 백그라운드 알림 추가
    
    - 앱이 hold된 상태가 아니더라도 알림이 보내지도록
    
    * refactor/notification-view-and-onboarding-hooby:
    알림 페이지 랜딩 변경 & 존재하지 않는 사용자 시 모달 표시 & 온보딩에서 취미 저장 형식 변경

--- src/components/notification/NotificationCard.js
+++ src/components/notification/NotificationCard.js
@@ -6,7 +6,9 @@ import { CustomTheme } from "@styles/CustomTheme";
 
 import IconAddFriend24 from "@components/Icon24/IconAddFriend24";
 import IconHeart24 from "@components/Icon24/IconHeart24";
+import { getProfileById } from "config/api";
 import { useNavigation } from "@react-navigation/native";
+import ModalKebabNotFoundMember from "@components/member/ModalKebabNotFoundMember";
 
 const { fontCaption, fontNavi } = CustomTheme;
 
@@ -19,9 +21,12 @@ const NotificationCard = ({
 }) => {
 	const navigation = useNavigation();
 	const [isRead, setIsRead] = useState(false);
+	const [modalVisible, setModalVisible] = useState(false);
 
 	let iconSvg;
 	if (type === "CONNECT") {
+		iconSvg = <IconAddFriend24 />;
+	} else if (type === "CONNECT_ACCEPT") {
 		iconSvg = <IconAddFriend24 />;
 	} else if (type === "POST") {
 		iconSvg = <IconHeart24 />;
@@ -49,7 +54,22 @@ const NotificationCard = ({
 			"true",
 		);
 		if (type === "CONNECT") {
+			console.log(typeId);
+			const response = await getProfileById(typeId);
+			console.log(response.data);
+			if (response.data.isDeleted) {
+				setModalVisible(true);
+				return;
+			}
 			navigation.navigate("ConnectProfilePage", { memberId: typeId });
+		} else if (type === "CONNECT_ACCEPT") {
+			const response = await getProfileById(typeId);
+			console.log(response.data);
+			if (response.data.isDeleted) {
+				setModalVisible(true);
+				return;
+			}
+			navigation.navigate("ConnectListPage", { screen: "그룹" });
 		} else if (type === "POST") {
 			navigation.navigate("PostPage", { postId: typeId });
 		} else {
@@ -58,20 +78,27 @@ const NotificationCard = ({
 	};
 
 	return (
-		<TouchableOpacity
-			style={[styles.rectangle, isRead && { opacity: 0.4 }]}
-			onPress={handleNotification}
-		>
-			<View style={styles.notify}>
-				<View style={styles.iconTextContainer}>
-					<View style={styles.icon}>{iconSvg}</View>
-					<View style={styles.textContainer}>
-						<Text style={styles.context}>{content}</Text>
+		<>
+			<ModalKebabNotFoundMember
+				modalVisible={modalVisible}
+				setModalVisible={setModalVisible}
+			/>
+
+			<TouchableOpacity
+				style={[styles.rectangle, isRead && { opacity: 0.4 }]}
+				onPress={handleNotification}
+			>
+				<View style={styles.notify}>
+					<View style={styles.iconTextContainer}>
+						<View style={styles.icon}>{iconSvg}</View>
+						<View style={styles.textContainer}>
+							<Text style={styles.context}>{content}</Text>
+						</View>
 					</View>
+					<Text style={styles.time}>{created}</Text>
 				</View>
-				<Text style={styles.time}>{created}</Text>
-			</View>
-		</TouchableOpacity>
+			</TouchableOpacity>
+		</>
 	);
 };
 
@@ -112,6 +139,35 @@ const styles = StyleSheet.create({
 		...fontNavi,
 		color: CustomTheme.textDisable,
 		marginRight: 28,
+	},
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "white",
+	},
+	modalContainer: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "rgba(0, 0, 0, 0.5)",
+	},
+	modalContent: {
+		width: 250,
+		padding: 20,
+		backgroundColor: "rgba(255, 255, 255, 0.7)",
+		borderRadius: 10,
+		alignItems: "center",
+	},
+	modalText: {
+		fontSize: 17,
+		marginBottom: 10,
+	},
+	modalDivider: {
+		width: "100%",
+		height: 1,
+		backgroundColor: "#ccc",
+		marginVertical: 5,
 	},
 });
 
