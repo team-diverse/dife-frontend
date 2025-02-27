73e3bfd8275aaaea2d7f763555c1e866e6b823e9
suyeon <stgood@kookmin.ac.kr>
2025-02-27 21:27:54 +0900

    feat/android-statusbar-5: private 변경 후 테스트

--- src/pages/community/CommunityStyles.jsx
+++ src/pages/community/CommunityStyles.jsx
@@ -7,7 +7,7 @@ const ChattingStyles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: CustomTheme.bgBasic,
-		// private 레포지토리 테스트
+		// private 레포지토리 테스트 - 2
 	},
 	backgroundBlue: {
 		position: "absolute",
