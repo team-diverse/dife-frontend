60c1975d494e9b5868a2a4ea1450fd989a4efce4
sooyeon_nine <124496650+KooSuYeon@users.noreply.github.com>
2025-04-11 06:31:30 +0900

    hotfix/accesspage-firstlaunch-securestore-3: (#261)
    - 알림 권한을 이미 허용한 사용자라면 알림 권한 허용 질의 페이지 보이지 않도록

--- src/pages/login/AccessPage.js
+++ src/pages/login/AccessPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, SafeAreaView } from "react-native";
 import * as Notifications from "expo-notifications";
 import { useNavigation } from "@react-navigation/native";
@@ -17,33 +17,44 @@ import GoBack from "@components/common/GoBack";
 const AccessPage = () => {
 	const { t } = useTranslation();
 	const navigation = useNavigation();
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const checkPermissions = async () => {
+			const { status: existingStatus } =
+				await Notifications.getPermissionsAsync();
+
+			if (existingStatus === "granted") {
+				await SecureStore.setItemAsync(
+					"notificationPermissionStatus",
+					existingStatus,
+				);
+				navigation.reset({
+					index: 0,
+					routes: [{ name: "Login" }],
+				});
+			} else {
+				setIsLoading(false);
+			}
+		};
+
+		checkPermissions();
+	}, []);
 
 	const requestPermissions = async () => {
-		const { status: existingStatus } =
-			await Notifications.getPermissionsAsync();
+		const { status } = await Notifications.requestPermissionsAsync();
+		await SecureStore.setItemAsync("notificationPermissionStatus", status);
 
-		if (existingStatus !== "granted") {
-			const { status } = await Notifications.requestPermissionsAsync();
-			await SecureStore.setItemAsync(
-				"notificationPermissionStatus",
-				status,
-			);
-
-			navigation.reset({
-				index: 0,
-				routes: [{ name: "LandingPage" }],
-			});
-		} else {
-			await SecureStore.setItemAsync(
-				"notificationPermissionStatus",
-				existingStatus,
-			);
-			navigation.replace("Login");
-		}
+		navigation.reset({
+			index: 0,
+			routes: [{ name: "LandingPage" }],
+		});
 	};
 
+	if (isLoading) return null;
+
 	return (
-		<SafeAreaView style={AccessStyles.container}>
+		<SafeAreaView style={[AccessStyles.container]}>
 			<GoBack />
 			<Text style={AccessStyles.textTitle}>{t("accessPageTitle")}</Text>
 			<View style={AccessStyles.containerContent}>
