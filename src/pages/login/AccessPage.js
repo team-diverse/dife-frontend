e48e27a2abef164528557269bb97e659edcc5d8b
sooyeon_nine <124496650+KooSuYeon@users.noreply.github.com>
2025-04-11 05:10:39 +0900

    hotfix/accesspage-firstlaunch-securestore-1: 앱 삭제 후 설치 시에 온보딩 페이지가 보이도록 (#257)
    * hotfix/accesspage-firstlaunch-securestore-1:
    
    - 앱이 처음 실행될 때 자동으로 실행되게 하려면 useEffect에 넣기
    - SecureStore 값이 null인지 정확히 확인한 다음에 navigation 흐름을 제어해야 하기 때문에, navigation.reset() 을 사용
    
    * hotfix/accesspage-firstlaunch-securestore-2:
    
    - 알림 granted는 한번 앱이 설치되면은 계속 삭제 후 재 설치 되더라도 남아있는 정보임
    - 이 정보가 setInitialRoute에서 덮어씌여져서 생겼던 LandingPage에 안보였던 문제였음

--- src/pages/login/AccessPage.js
+++ src/pages/login/AccessPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, SafeAreaView } from "react-native";
 import * as Notifications from "expo-notifications";
 import { useNavigation } from "@react-navigation/native";
@@ -28,16 +28,27 @@ const AccessPage = () => {
 				console.warn("알림 권한이 거부되었습니다.");
 			}
 		}
+		await new Promise((resolve) => setTimeout(resolve, 300));
 
-		const firstLaunch = await SecureStore.getItem("hasLaunched");
+		const firstLaunch = await SecureStore.getItemAsync("hasLaunched");
 
 		if (firstLaunch === null) {
-			navigation.navigate("LandingPage");
 			await SecureStore.setItemAsync("hasLaunched", "true");
+			navigation.reset({
+				index: 0,
+				routes: [{ name: "LandingPage" }],
+			});
 		} else {
-			navigation.replace("Login");
+			navigation.reset({
+				index: 0,
+				routes: [{ name: "Login" }],
+			});
 		}
 	};
+
+	useEffect(() => {
+		requestPermissions();
+	}, []);
 
 	return (
 		<SafeAreaView style={[AccessStyles.container]}>
