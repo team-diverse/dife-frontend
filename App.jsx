bd848b2858aff44604efb70bb03b5cc4ec86c927
sooyeon_nine <124496650+KooSuYeon@users.noreply.github.com>
2025-06-09 19:36:21 +0900

    Feat/auto verification : 갱신된 애플 개발자 계정 provision profile 업데이트 해 build failed 해결 시도 (#277)
    * Feat/auto verification : 갱신된 애플 개발자 계정 provision profile 업데이트 해 build failed 해결 시도 (#275)
    
    * feat/auto-verification-1: 학생증 자동 인증 프론트 로직 개발
    
    - OnboardingStep6Page : 온보딩 Step에서 프로필 완료 페이지 위치 변경 (파일 두 개 업로드가 진행되기까지 시간이 너무 오래 걸려서)
    - LoadingVerificationPage : 해당 페이지는 프로필 완료 페이지 이후에 보이는 페이지로, 실제 학생증 진위여부에 따라 업데이트 되는 페이지임 5초마다 api 요청을 보내 진위여부를 확인하며, 진행되는 문구가 바뀜
    
    - else : 최근 업데이트 된 SDK 53 버전에 따라 맞는 환경 설정 업데이트
    (translation 관련 버전 업데이트의 이슈로 인한 업데이트)
    
    * feat/auto-verification-2: 갱신된 애플 개발자계정 적용 해 build failed 해결 시도
    
    * Feat/auto verification-3 : Lint Test

--- App.jsx
+++ App.jsx
@@ -7,7 +7,6 @@ import { useFonts } from "expo-font";
 import * as Notifications from "expo-notifications";
 import * as SecureStore from "expo-secure-store";
 import { I18nextProvider } from "react-i18next";
-
 import { PostModifyProvider } from "src/states/PostModifyContext";
 import { AuthProvider, useAuth } from "src/states/AuthContext";
 import { getMyProfile } from "config/api";
