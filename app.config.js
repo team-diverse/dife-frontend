d76d093f98535292e3550c201ebe4ca840848edb
sooyeon_nine <124496650+KooSuYeon@users.noreply.github.com>
2025-06-09 20:01:11 +0900

    Feat/auto verification-3 : Lint Test (#278)
    * Feat/auto verification-3 : Lint Test
    
    * Feat/auto verification-4 : Staging 과 diff 수정
    
    * Feat/auto verification-4 : Staging 과 diff 수정 - Lint test
    
    * merge/06_09-2: 앱 제출을 위한 버전 수정

--- app.config.js
+++ app.config.js
@@ -30,7 +30,7 @@ export default {
 	expo: {
 		name: "Dife",
 		slug: "dife",
-		version: "1.0.0",
+		version: "1.0.2",
 		owner: "team-diverse",
 		orientation: "portrait",
 		icon: "src/assets/icon.png",
