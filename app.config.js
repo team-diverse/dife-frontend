98ccfd09e8b775d4385566c3cb10d01706a51a44
suyeon <stgood@kookmin.ac.kr>
2025-09-17 12:13:36 +0900

    merge/09_17-1: update version to 1.0.4

--- app.config.js
+++ app.config.js
@@ -30,7 +30,7 @@ export default {
 	expo: {
 		name: "Dife",
 		slug: "dife",
-		version: "1.0.3",
+		version: "1.0.4",
 		owner: "team-diverse",
 		orientation: "portrait",
 		icon: "src/assets/icon.png",
