faf8a1350501c830e715ed9f9517c211425d2cd6
nnyouung <211_0@kookmin.ac.kr>
2025-10-30 22:59:10 +0900

    fix: 신고하기 버튼 텍스트 정렬 문제 해결

--- src/components/Report.js
+++ src/components/Report.js
@@ -200,7 +200,6 @@ const styles = StyleSheet.create({
 		right: 20,
 	},
 	buttonNo: {
-		flex: 1,
 		width: 102,
 		padding: 10,
 		backgroundColor: CustomTheme.bgBasic,
@@ -213,9 +212,9 @@ const styles = StyleSheet.create({
 	},
 	textNo: {
 		...fontSub14,
+		textAlign: "center"
 	},
 	buttonReport: {
-		flex: 1,
 		width: 102,
 		padding: 10,
 		backgroundColor: CustomTheme.warningRed,
@@ -229,6 +228,7 @@ const styles = StyleSheet.create({
 	textReport: {
 		...fontSub14,
 		color: CustomTheme.bgBasic,
+		textAlign: "center"
 	},
 });
 
