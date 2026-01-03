065bcf39867e6c93722171177ce8dbdbfb9a66bd
sooyeon_nine <124496650+KooSuYeon@users.noreply.github.com>
2026-01-04 01:31:41 +0900

    revert-and-hotfix/update-sentry-auth-token : 문제가 아니었던 expo-build-properties 버전 되돌리기 및 업데이트 된 Sentry Auth Token 사용 (#309)
    * revert-and-hotfix/update-sentry-auth-token : 문제가 아니었던 expo-build-properties 버전 되돌리기 및 업데이트 된 Sentry Auth Token 사용
    
    * revert-and-hotfix/update-sentry-auth-token-2 : build 오류 수정 시도

--- metro.config.mjs
+++ metro.config.mjs
@@ -1,4 +1,9 @@
-import { getSentryExpoConfig } from "@sentry/react-native/metro";
+import { getSentryExpoConfig } from "@sentry/react-native/metro.js";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const config = getSentryExpoConfig(__dirname);
 
