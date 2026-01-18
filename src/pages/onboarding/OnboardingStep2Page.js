044c7e94a08bd053649f2f06c15704cdc4c2a0a4
sooyeon_nine <124496650+KooSuYeon@users.noreply.github.com>
2026-01-18 13:11:49 +0900

    hotfix/update-member-nation-and-languages-1 : 최초 온보딩에서 누락되었던 languages와 nation 문제를 해결한다. (#315)
    - Step2 (nation 누락)
    - setNotion 오타 수정, handleProfileSubmit에서 데이터 누락 문제 수정
    
    - Step5 (lanauges누락)
    - React의 비동기적 상태 업데이트로 인해 서버 전송 시 이전 데이터가 포함되던 로직을 로컬 변수 직접 참조 방식으로 변경하여 해결

--- src/pages/onboarding/OnboardingStep2Page.js
+++ src/pages/onboarding/OnboardingStep2Page.js
@@ -29,7 +29,9 @@ const OnboardingStep2Page = ({ goToNext, saveData, stepData }) => {
 
 	const [image, setImage] = useState(stepData[2].image || null);
 	const [bio, setBio] = useState(stepData[2].bio || "");
-	const [nation, setNotion] = useState(stepData[2].selectedCountry || "");
+	const [nation, setNation] = useState(
+		stepData[2].nation || stepData[2].selectedCountry || "",
+	);
 
 	const handleKeyboard = () => {
 		Keyboard.dismiss();
@@ -59,11 +61,13 @@ const OnboardingStep2Page = ({ goToNext, saveData, stepData }) => {
 	};
 
 	useEffect(() => {
-		setNotion(stepData[2].selectedCountry);
+		if (stepData[2].selectedCountry) {
+			setNation(stepData[2].selectedCountry);
+		}
 	}, [stepData[2].selectedCountry]);
 
 	const handleProfileSubmit = () => {
-		saveData(2, { image, bio });
+		saveData(2, { image, bio, nation: nation });
 		goToNext(3);
 	};
 
